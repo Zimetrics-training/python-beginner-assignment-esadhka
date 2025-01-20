232d3165a91c123a66b9d226c6be64b1baffdbe1
esadhka <esadhka@gmail.com>
2025-01-20 17:45:34 +0530

    Update bank_account.py

--- src/bank_account.py
+++ src/bank_account.py
@@ -15,6 +15,8 @@ Returns the current balance of the account.
 Transfers the specified amount to another BankAccount instance. If the amount exceeds the balance or is less than or equal to zero, it raises an InsufficientFundsError or ValueError, respectively.
 6. __str__(self):
 Returns a string representation of the account in the format: AccountHolder: {account_holder_name}, Balance: {current_balance}.
+7. __eq__(self, other):
+Compares two BankAccount objects. Two accounts are considered equal if their account holders and balances are the same.
 
 Custom Exception:
 Define a custom exception InsufficientFundsError that will be raised when an account has insufficient funds for a withdrawal or transfer.
@@ -26,24 +28,37 @@ class InsufficientFundsError(Exception):
 class BankAccount:
     def __init__(self, account_holder: str, initial_balance: float):
         # Initialize the bank account
-        pass
+        self.account_holder = account_holder
+        self.initial_balance = initial_balance
 
     def deposit(self, amount: float):
         # Deposit amount to the account
-        pass
+        if amount<0:
+            raise ValueError("You can't deposit negative amount!")
+        self.initial_balance = self.initial_balance+amount
 
     def withdraw(self, amount: float):
         # Withdraw amount from the account
-        pass
+        if amount<0:
+            raise ValueError("You can't withdraw negative amount!")
+        if self.initial_balance<amount:
+            raise InsufficientFundsError("You can't withdraw amount greater than balance!")
+        self.initial_balance = self.initial_balance-amount
 
     def get_balance(self):
         # Return current account balance
-        pass
+        return self.initial_balance
 
     def transfer(self, recipient_account, amount: float):
         # Transfer amount to another account
-        pass
+        if amount<0:
+            raise ValueError("You can't transfer negative amount!")
+        if self.initial_balance<amount:
+            raise InsufficientFundsError("You can't transfer amount greater than account balance!")
+        self.initial_balance = self.initial_balance-amount
+        recipient_account.initial_balance = recipient_account.initial_balance+amount
+        #print(f"Transfered {amount} to {recipient_account}!")
 
     def __str__(self):
         # Return the string representation of the account
-        pass
+        return f"AccountHolder: {self.account_holder}, Balance: {self.initial_balance}"
